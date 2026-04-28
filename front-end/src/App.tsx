67275a372abd29f9fd466083050b3382160cf971
Monty <bgwhiskey64@gmail.com>
2026-04-28 18:10:49 +0800

    Organize project: Added pages folder and modularized directory

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router'
+import { HomePage } from './pages/homepage/HomePage'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <Routes>
+      <Route index element={<HomePage />}/>
+      {/* <Route index element={<ServicesPage />}/>
+      <Route index element={<ProductsPage />}/>
+      <Route index element={<BookPage />}/>
+      <Route index element={<CartPage />}/>
+      <Route index element={<AccountPage />}/> */}
+    </Routes>
   )
 }
 

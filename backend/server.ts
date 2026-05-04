a12ed3f08de1fbd48c2743f963c704e492b19ba9
Monty <bgwhiskey64@gmail.com>
2026-05-04 21:51:19 +0800

    added render's environment

--- backend/server.ts
+++ backend/server.ts
@@ -25,7 +25,7 @@ mongoose.connect(mongoURI)
     .catch((err) => console.error('❌ Connection error:', err));
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
